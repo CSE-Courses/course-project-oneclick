5f459505afbc0a85cbb3525fe6ef9496752d2283
Jimmy <cyncatron@gmail.com>
2020-10-18 23:02:37 -0400

    change eventschedualer to be a method

--- gui_popup.py
+++ gui_popup.py
@@ -3,15 +3,15 @@ from tkinter import CENTER
 from tkinter import ttk
 import os
 import gi
-from gi.repository import Notify
 gi.require_version('Notify', '0.7')
+from gi.repository import Notify
 TITLE_FONT= ("Verdana", 20)
 BODY_FONT = ("Verdana", 12)
 def run_on_click(popup,command):
     popup.destroy()
     os.system(command)
-def zoom_call_command(link):
-    return "zoom --url="+link
+def zoom_call_command(link, zoom_path):
+    return f"{zoom_path} --url="+link
 
 def event_pop_up(event_title,desc,command):
     popup = tk.Tk()
@@ -28,10 +28,10 @@ def event_pop_up(event_title,desc,command):
 
 def notification_start(event_name,time_till):
     Notify.init(event_name)
-    notification = Notify.Notification(summary=event_name) 
+    notification = Notify.Notification(summary=f"{event_name}|in {time_till} minutes")
 
     notification.show()
 
-notification_start("zoom","stuff")
+notification_start("zoom","5")
 #event_pop_up("zoom call","sta301","termite")
 
